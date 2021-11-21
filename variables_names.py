153a426e9ea40a0bfb7d8fb56bda03bf3352d1c4
Anton Kovalev <acetonen@gmail.com>
2021-11-21 14:06:54 +0300

    1 Variable names

---
+++ variables_names.py
@@ -0,0 +1,35 @@
+# 1. df - rooms_data_frame
+# Таблица данных комнат
+
+# 2. pmi_shop - projected_monthly_income_per_shop
+# Предположительный месячный доход с магазина
+
+# 3. img - link_to_image
+# Ссылка на картинку
+
+# 4. s_date - start_date_of_rent
+# Дата начала аренды
+
+# 5. sys_name - product_system_name
+# Системное имя продукта
+
+# 6. pac - post_acquisition_checklist
+# Чеклист после приобретения
+
+# 7. vac_cost - vacancies_cost
+# Стоимость вакансий
+
+# 8. avg_sum - average_summary_of_income
+# Средняя сумма входящих
+
+# 9. ttl_money - total_money_sum
+# Полная сумма денег
+
+# 10. comp_avg - average_among_comparables
+# Среднее от сравнений
+
+# 11. target - target_monthly_income
+# Целевой месячный доход
+
+# 12. sum - summary_of_profits
+# Сумма доходов
