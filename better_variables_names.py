9ffd24ff3f1ad94af08c4038207b71d07b4b9936
Anton Kovalev <acetonen@gmail.com>
2021-11-27 15:37:06 +0300

    2 Better variables names

---
+++ better_variables_names.py
@@ -0,0 +1,23 @@
+# 6.1 levels of abstraction:
+#   costs -> average_money_income
+#   title -> neighborhood_name
+#   sum -> monthly_outcome
+#   total -> result_of_taxes
+#   template_context -> customer_data
+
+# 6.2 CS terms in names:
+#   webhooks -> webhook_queue
+#   stripe_integration -> stripe_facade
+#   payment_processor -> payment_factory
+#   filter_string -> query_string
+
+# 6.3 context in names:
+#   number -> apartments_number
+#   phone -> lead_phone
+#   email -> user_email
+
+# 6.4 optimal names length 8-20 symbols:
+#   costs -> average_money_income
+#   title -> neighborhood_name
+#   total -> result_of_taxes
+#   residence_as_entire_apartment_with_furnishing -> furnishing_residence
